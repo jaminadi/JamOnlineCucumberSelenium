079ff046280cf2378e3041897988ced74f9bc127
jaminadi <jama.nadyrbek@gmail.com>
2022-04-12 09:50:46 -0500

    Create Car feature

--- src/test/java/com/vytrack/runners/ActivitiesTestRunner.java
+++ src/test/java/com/vytrack/runners/ActivitiesTestRunner.java
@@ -11,10 +11,10 @@ import org.junit.runner.RunWith;
             features = "src/test/resources/features/activities",
             dryRun = false,
             strict = false,
-            tags = "@view_calendar_events",
+            tags = "",
             plugin = {
                     "html:target/default-report",
-                    "json:target/cucumber.json"
+                    "json:target/cucumber2.json"
             }
     )
     public class ActivitiesTestRunner {
--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         dryRun = false,
         strict = false,
-        tags = "@create_calendar_event_2",
+        tags = "@login",
         plugin = {
                 "html:target/default-report",
                 "json:target/cucumber1.json"
