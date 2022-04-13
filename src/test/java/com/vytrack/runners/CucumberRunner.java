05bcd1e8f07ec2d3ed5e3c2a76c3fbd55168ad4e
jaminadi <jama.nadyrbek@gmail.com>
2022-04-13 15:15:45 -0500

    Updated CreateCar feature file

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         dryRun = false,
         strict = false,
-        tags = "@scenario_outline_2",
+        tags = "@create_car_ddt",
         plugin = {
                 "html:target/default-report",
                 "json:target/cucumber1.json"
