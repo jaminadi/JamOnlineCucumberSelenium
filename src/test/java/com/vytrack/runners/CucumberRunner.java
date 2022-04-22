c6def86a2a55fc757037c2cb7b83272457814600
jaminadi <jama.nadyrbek@gmail.com>
2022-04-22 08:46:59 -0500

    Added Smoke test feature file

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         dryRun = false,
         strict = false,
-        tags = "@driver",
+    //    tags = "@driver",
         plugin = {
                 "html:target/default-report",
                 "json:target/cucumber1.json"
